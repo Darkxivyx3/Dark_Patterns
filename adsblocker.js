762fab6dac9ff7a3bd12263e34c9507d25812882
Darakhshan Zehra <122298615+Darkxivyx3@users.noreply.github.com>
2024-01-29 23:02:25 +0530

    Add files via upload

---
+++ adsblocker.js
@@ -0,0 +1,10 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        return{cancel: true};
+    },
+    {urls:[
+        "*://www.youtube.com/*"
+
+    ]},
+    ("blocking")
+)
